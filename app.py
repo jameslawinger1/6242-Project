e152eca598965c3668b9bcf581c4e04f92d308ec
James Lawinger <jameslawinger@gmail.com>
2025-04-07 14:48:36 -0500

    added features

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle 
+import pickle
+import datetime
 
 
 with(open('rf_model.pkl', 'rb')) as f: 
@@ -18,30 +19,43 @@ with open('city_coords.pkl', 'rb') as f:
 
 st.title("Airbnb Price Estimator")
 
+#City selection and coordinates
 city = st.selectbox("Select City", sorted(city_coords.keys()))
-
 lat_default = city_coords[city]['latitude']
 lon_default = city_coords[city]['longitude']
-
+#get coordinates from city
 latitude = st.number_input("Latitude", value=lat_default, format="%.6f")
 longitude = st.number_input("Longitude", value=lon_default, format="%.6f")
+#map it
+st.map(pd.DataFrame([{
+    'lat': latitude,
+    'lon': longitude
+}]))
 
 bedrooms = st.slider("Bedrooms", 0, 10, 1)
 bathrooms = st.slider("Bathrooms", 0, 5, 1)
 tenure = st.slider("Host Tenure (Years)", 0, 20, 1)
-num_amenities = st.slider("Number of Amenities", 0, 50, 10)
 accommodates = st.slider("Accommodates", 1, 16, 4)
-longitude = st.number_input("Longitude", value=-87.6298)  
-latitude = st.number_input("Latitude", value=41.8781)
+
+stay_date = st.date_input("Stay Date", datetime.date.today())
+days_since = (datetime.date.today() - stay_date).days
+
+with st.expander("Advanced Listing Settings"):
+    host_listings = st.slider("Host's Total Listings", 0, 100, value=6)
+    reviews_per_month = st.slider("Reviews Per Month", 0.0, 10.0, value=1.0, step=0.1)
+    minimum_nights = st.slider("Avg Minimum Nights", 1, 60, value=3)
 
 input_dict = {
     'bedrooms': bedrooms,
     'num_bathrooms': bathrooms,
     'tenure': tenure,
-    'num_amenities': num_amenities,
     'accommodates': accommodates,
     'latitude': latitude,
-    'longitude': longitude
+    'longitude': longitude,
+    'date': days_since,
+    'host_total_listings_count': host_listings,
+    'reviews_per_month': reviews_per_month,
+    'minimum_nights_avg_ntm': minimum_nights
 }
 X_input = pd.DataFrame([input_dict])
 
